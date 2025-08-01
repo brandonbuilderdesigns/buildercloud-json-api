c09ce4854db9e4821f48a9084b1d1bc689e37e57
Brandon Barelmann <brandonbarelmann@brandons-mbp-2.lan>
2025-08-01 16:15:39 -0500

    fix: debug JSON handler

--- api/feed.js
+++ api/feed.js
@@ -1,31 +1,5 @@
 export default function handler(req, res) {
-  try {
-    res.status(200).json({
-      Builders: {
-        Corporation: [
-          {
-            CorporateBuilderNumber: ["AspenHomesTemplate"],
-            CorporateState: ["KS"],
-            CorporateName: ["Aspen Homes Template"],
-            CorporateReportingEmail: ["feeds@builderdesigns.com"],
-            Specialties: [
-              {
-                Specialty: [
-                  {
-                    "_": "1",
-                    "$": {
-                      Type: "CustomHomes"
-                    }
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      }
-    });
-  } catch (err) {
-    console.error("❌ JSON error:", err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+  res.status(200).json({
+    message: "It works!"
+  });
 }
